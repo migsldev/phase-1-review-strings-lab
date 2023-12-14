1319cd55d60c7a20e20b186a5beff45604f5f7e7
Miguel Leonar <migsldev@gmail.com>
2023-12-14 23:07:31 +1100

    updated string labs passed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Miguel';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser[0];
+
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = `Welcome, ${firstInitial} !`;
+
+console.log(shortGreeting);
